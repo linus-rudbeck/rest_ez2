ebf91d1454863c41acf01568468cf241418b0924
linus-rudbeck <linus.rudbeck@distansakademin.se>
2025-05-26 13:42:19 +0200

    Update code

--- src/main/java/se/distansakademin/rest_ez/MainController.java
+++ src/main/java/se/distansakademin/rest_ez/MainController.java
@@ -3,6 +3,8 @@ package se.distansakademin.rest_ez;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// .
+
 @RestController
 public class MainController {
 
